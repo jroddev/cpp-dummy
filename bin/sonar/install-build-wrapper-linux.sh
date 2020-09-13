99be23cdb8177a45b29204f954db55e3d3294bb6
jroddev <jroddev@gmail.com>
2020-09-13 19:51:29 +1000

    fix path

--- bin/sonar/install-build-wrapper-linux.sh
+++ bin/sonar/install-build-wrapper-linux.sh
@@ -6,5 +6,5 @@ SONAR_BUILD_WRAPPER_BIN=$SONAR_BUILD_WRAPPER/build-wrapper-$OS-x86/bin
 mkdir -p $SONAR_BUILD_WRAPPER
 curl -Ol https://sonarcloud.io/static/cpp/build-wrapper-linux-x86.zip
 unzip -qq -o build-wrapper-linux-x86.zip -d $SONAR_BUILD_WRAPPER
-chmod +x $SONAR_BUILD_WRAPPER_BIN/build-wrapper-linux-x86
+chmod +x $SONAR_BUILD_WRAPPER_BIN/build-wrapper-linux-x86-64
 export PATH=$PATH:$SONAR_BUILD_WRAPPER_BIN
