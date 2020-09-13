b73b079a0e3dd769c927353b0591402fd3915ac7
jroddev <jroddev@gmail.com>
2020-09-13 19:59:01 +1000

    remove unused variables and unzip in current dir

--- bin/sonar/install-build-wrapper-linux.sh
+++ bin/sonar/install-build-wrapper-linux.sh
@@ -1,10 +1,6 @@
 #!/bin/sh
 
 set -e
-SONAR_BUILD_WRAPPER=/tmp/cache/build-wrapper
-SONAR_BUILD_WRAPPER_BIN=$SONAR_BUILD_WRAPPER/build-wrapper-linux-x86
-mkdir -p $SONAR_BUILD_WRAPPER
 curl -Ol https://sonarcloud.io/static/cpp/build-wrapper-linux-x86.zip
-unzip -qq -o build-wrapper-linux-x86.zip -d $SONAR_BUILD_WRAPPER
-chmod +x $SONAR_BUILD_WRAPPER_BIN/build-wrapper-linux-x86-64
-export PATH=$PATH:$SONAR_BUILD_WRAPPER_BIN
+unzip -qq -o build-wrapper-linux-x86.zip
+chmod + xbuild-wrapper-linux-x86/build-wrapper-linux-x86-64
