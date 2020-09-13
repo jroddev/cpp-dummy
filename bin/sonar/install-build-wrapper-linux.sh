600dd5446ba97998ce2b0a9455ba0026748558ef
jroddev <jroddev@gmail.com>
2020-09-13 20:22:53 +1000

    Add x flag

--- bin/sonar/install-build-wrapper-linux.sh
+++ bin/sonar/install-build-wrapper-linux.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-set -e
+set -ex
 curl -Ol https://sonarcloud.io/static/cpp/build-wrapper-linux-x86.zip
 unzip -qq -o build-wrapper-linux-x86.zip
 chmod +x build-wrapper-linux-x86/build-wrapper-linux-x86-64
