665303e8d85c23f70f146163a6ce53201dc4a692
jroddev <jroddev@gmail.com>
2020-09-13 19:43:32 +1000

    make dir

--- bin/sonar/install-build-wrapper-linux.sh
+++ bin/sonar/install-build-wrapper-linux.sh
@@ -3,6 +3,7 @@
 set -e
 SONAR_BUILD_WRAPPER=/tmp/cache/build-wrapper
 SONAR_BUILD_WRAPPER_BIN=$SONAR_BUILD_WRAPPER/build-wrapper-$OS-x86/bin
+mkdir -p $SONAR_BUILD_WRAPPER
 curl -Ol https://sonarcloud.io/static/cpp/build-wrapper-linux-x86.zip
 unzip -qq -o build-wrapper-linux-x86.zip -d $SONAR_BUILD_WRAPPER
 chmod +x $SONAR_BUILD_WRAPPER_BIN/build-wrapper-linux-x86
