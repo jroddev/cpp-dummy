a0255b5abaa4cadd27043570167cd39c0e896963
jroddev <jroddev@gmail.com>
2020-09-13 19:38:57 +1000

    use handrolled script to install sonar build wrapper

---
+++ bin/sonar/install-build-wrapper-linux.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+set -e
+SONAR_BUILD_WRAPPER=/tmp/cache/build-wrapper
+SONAR_BUILD_WRAPPER_BIN=$SONAR_BUILD_WRAPPER/build-wrapper-$OS-x86/bin
+curl -Ol https://sonarcloud.io/static/cpp/build-wrapper-linux-x86.zip
+unzip -qq -o build-wrapper-linux-x86.zip -d $SONAR_BUILD_WRAPPER
+chmod +x $SONAR_BUILD_WRAPPER_BIN/build-wrapper-linux-x86
+export PATH=$PATH:$SONAR_BUILD_WRAPPER_BIN
